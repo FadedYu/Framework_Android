b2e42750073b5e5483811b83206cf47f546aa5af
LY <15677355462@163.com>
2019-04-23 17:31:26 +0800

    FileCallback2文件后台下载添加到通知栏显示

--- app/src/main/java/com/bonait/bnframework/common/http/callback/files2/FileProgressDialogCallBack2.java
+++ app/src/main/java/com/bonait/bnframework/common/http/callback/files2/FileProgressDialogCallBack2.java
@@ -4,11 +4,19 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.text.format.Formatter;
+import android.util.Log;
 
 import com.bonait.bnframework.application.MainApplication;
+import com.bonait.bnframework.common.notification.DownloadNotification;
+import com.bonait.bnframework.common.utils.AlertDialogUtils;
+import com.bonait.bnframework.common.utils.NetworkUtils;
+import com.bonait.bnframework.common.utils.ToastUtils;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.model.Progress;
+import com.lzy.okgo.model.Response;
 import com.lzy.okgo.request.base.Request;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
 
 import java.io.File;
 import java.util.Locale;
@@ -20,21 +28,22 @@ import java.util.Locale;
 public abstract class FileProgressDialogCallBack2 extends FileCallback2 {
 
     private ProgressDialog progressDialog;
+    private DownloadNotification downloadNotification;
 
 
     public FileProgressDialogCallBack2(Context context, long totalSize) {
         super(totalSize);
-        initDialog(context,totalSize);
+        initDialog(context, totalSize);
     }
 
     public FileProgressDialogCallBack2(Context context, String destFileName, long totalSize) {
         super(destFileName, totalSize);
-        initDialog(context,totalSize);
+        initDialog(context, totalSize);
     }
 
     public FileProgressDialogCallBack2(Context context, String destFileDir, String destFileName, long totalSize) {
         super(destFileDir, destFileName, totalSize);
-        initDialog(context,totalSize);
+        initDialog(context, totalSize);
     }
 
     @Override
@@ -49,6 +58,23 @@ public abstract class FileProgressDialogCallBack2 extends FileCallback2 {
         if (progressDialog != null && progressDialog.isShowing()) {
             progressDialog.dismiss();
         }
+
+        if (downloadNotification != null) {
+            downloadNotification.downloadComplete();
+        }
+    }
+
+    @Override
+    public void onError(Response<File> response) {
+        super.onError(response);
+        if (downloadNotification != null) {
+            downloadNotification.downloadError();
+        }
+
+        ToastUtils.error(response.body().getName() + "下载失败，请重新下载！");
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
     }
 
     @Override
@@ -57,20 +83,44 @@ public abstract class FileProgressDialogCallBack2 extends FileCallback2 {
         String totalLength = Formatter.formatFileSize(MainApplication.getContext(), progress.totalSize);
 
         progressDialog.setProgress((int) (progress.fraction * progress.totalSize));
-        progressDialog.setProgressNumberFormat(String.format(Locale.CHINA,"%sMb / %sMb",downloadLength,totalLength));
+        progressDialog.setProgressNumberFormat(String.format(Locale.CHINA, "%sMb / %sMb", downloadLength, totalLength));
+        Log.d("downloadProgress", progress.fileName + " 下载中……" + String.format(Locale.CHINA, "%s / %s", downloadLength, totalLength));
+
+        if (downloadNotification != null) {
+            downloadNotification.showProgress(progress);
+        }
+    }
+
+    /**
+     * 判断网络状态。移动/WiFi，弹出警告框
+     */
+    private void initDialog(final Context context, final long totalSize) {
+        if (NetworkUtils.isActiveNetworkMobile(context)) {
+            String title = "温馨提示！";
+            String message = "当前网络为移动网络，可能会消耗大量移动流量数据，确定要下载吗？";
+            AlertDialogUtils.showDialog(context, title, message, new QMUIDialogAction.ActionListener() {
+                @Override
+                public void onClick(QMUIDialog dialog, int index) {
+                    downloadingDialog(context, totalSize);
+                    dialog.dismiss();
+                }
+            });
+        } else {
+            downloadingDialog(context, totalSize);
+        }
     }
 
     /**
      * 显示进度条对话框
-     */
-    private void initDialog(final Context context, long totalSize) {
+     * */
+    private void downloadingDialog(final Context context, long totalSize) {
         progressDialog = new ProgressDialog(context);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setCancelable(false);
         progressDialog.setCanceledOnTouchOutside(false);
         progressDialog.setTitle("正在下载");
 
-        if (totalSize>0) {
+        if (totalSize > 0) {
             progressDialog.setMax((int) totalSize);
         }
 
@@ -78,6 +128,7 @@ public abstract class FileProgressDialogCallBack2 extends FileCallback2 {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 OkGo.getInstance().cancelTag(context);
+                ToastUtils.info("已停止下载！");
                 dialog.dismiss();
             }
         });
@@ -85,7 +136,8 @@ public abstract class FileProgressDialogCallBack2 extends FileCallback2 {
         progressDialog.setButton(DialogInterface.BUTTON_POSITIVE, "后台下载", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                downloadNotification = new DownloadNotification(context);
+                dialog.dismiss();
             }
         });
     }
